71a4d48d1a2c744fbb0d3ca72f3098628f4e4b6f
Vahe Karapetyan <vzkarapetyan@gmail.com>
2019-12-13 01:21:02 +0400

    Updates

---
+++ precache-manifest.3bf7f22efe47cb6f9f041789d61f5f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc94e89f08d379aca84bc9157f1f4dc",
+    "url": "/project-structure/index.html"
+  },
+  {
+    "revision": "7fac5b4e35c96285c76e",
+    "url": "/project-structure/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "c4dd9ca4597e4a171892",
+    "url": "/project-structure/static/css/main.97e514fa.chunk.css"
+  },
+  {
+    "revision": "7fac5b4e35c96285c76e",
+    "url": "/project-structure/static/js/2.afbfbb8e.chunk.js"
+  },
+  {
+    "revision": "c4dd9ca4597e4a171892",
+    "url": "/project-structure/static/js/main.7a7379e5.chunk.js"
+  },
+  {
+    "revision": "ff48843dfbe209a64e64",
+    "url": "/project-structure/static/js/runtime-main.0d5ff64a.js"
+  }
+]);
